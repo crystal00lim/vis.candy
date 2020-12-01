1e46d822cf065b35458ddc833a50af626d8fe989
crystal00lim <crystalbb00@gmail.com>
2020-12-01 23:02:34 +0000

    styled navbar icon, but cannot get links to work

--- script.js
+++ script.js
@@ -36,10 +36,16 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    document.getElementById("navbar").style.padding = "30px 10px";
-    document.getElementById("logo").style.fontSize = "25px";
+    document.getElementById("navbar").style.padding = "10px 5px";
+    document.getElementById("logo").style.fontSize = "20px";
   } else {
     document.getElementById("navbar").style.padding = "80px 10px";
     document.getElementById("logo").style.fontSize = "35px";
   }
 }
+
+$(document).ready(function () {
+  $('.first-button').on('click', function () {
+    $('.animated-icon1').toggleClass('open');
+  });
+});
