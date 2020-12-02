83ad605534d4f74566ec92022b959a0a31e1bcc6
crystal00lim <crystalbb00@gmail.com>
2020-12-02 20:45:24 +0000

    balance

--- script.js
+++ script.js
@@ -32,14 +32,10 @@ function dark() {
 location.reload();
 }
 
-window.onscroll = function() {scrollFunction()};
+// scroll function is in resume-1 repl. 
 
-function scrollFunction() {
-  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    document.getElementById("navbar").style.padding = "30px 10px";
-    document.getElementById("logo").style.fontSize = "25px";
-  } else {
-    document.getElementById("navbar").style.padding = "80px 10px";
-    document.getElementById("logo").style.fontSize = "35px";
-  }
-}
+$(document).ready(function () {
+  $('.first-button').on('click', function () {
+    $('.animated-icon1').toggleClass('open');
+  });
+});
